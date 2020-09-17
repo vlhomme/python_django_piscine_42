1c552e72f0dbcfcc2b4c70320aff18f85b1c13c2
nepriel <virgile.lhomme@gmx.fr>
2020-09-17 10:00:13 +0200

    ex05

--- d01/ex05/all_in.py
+++ d01/ex05/all_in.py
@@ -13,6 +13,26 @@ if __name__ == '__main__':
             "NJ": "Trenton",
             "CO": "Denver"
             }
-    if len(sys.argv) >= 2:
-        for arg in list:
-            if 
+    if len(sys.argv) >= 2 and len(sys.argv) < 1000:
+        bigStringOfArg = (''.join(sys.argv[1]))
+        # print(bigStringOfArg)
+        for getArg in bigStringOfArg.split(','):
+            arg = getArg.strip()
+            if len(arg) != 0:
+                #we have a state
+                if arg.lower() in (name.lower() for name in states):
+                    theCapital = (capital_cities[states[arg.lower().title()]])
+                    print(theCapital, " is the capital of ", arg.lower().title())
+                #we have a city
+                elif arg.lower() in (name.lower() for name in capital_cities.values()):
+                    stateShort = ''
+                    for key, value in capital_cities.items():
+                        if value.lower() == arg.lower():
+                            stateShort = key
+                    state = ''
+                    for key, value in states.items():
+                        if value == stateShort:
+                            state = key
+                    print(arg.title(), " is the capital of", state)
+                else:
+                    print(arg, " is neither a capital city nor a state")
