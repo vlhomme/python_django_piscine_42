7dd678be7ac70b3b2bf2e75e3d1f5abcd4197d13
nepriel <leherisson@gmx.fr>
2020-11-22 13:53:28 +0100

    last exercise of day03

--- d03/ex05/mysite/mysite/urls.py
+++ d03/ex05/mysite/mysite/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
 
 urlpatterns = [
-    path('helloworld/', include('polls.urls')),
+    path('helloworld/', include('helloworld.urls')),
     path('admin/', admin.site.urls),
 ]
